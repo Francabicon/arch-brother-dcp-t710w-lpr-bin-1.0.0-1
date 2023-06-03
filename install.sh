52545881e85a6e32eabdc1b4790c486c3dfb6ebf
Francabicon <52349593+Francabicon@users.noreply.github.com>
2023-06-03 11:33:41 +0800

    Update install.sh (#4)
    Signed-off-by: Francabicon <52349593+Francabicon@users.noreply.github.com>

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ fi
 
 
 #install cups common
-echo "installing Brother Cups Commons and scanner dependencies ..."
+echo "installing Brother Cups Commons and Scanner dependencies ..."
 
 yay -S brscan4 brscan-skey brother-cups-wrapper-common
 
@@ -24,3 +24,8 @@ yay -S brscan4 brscan-skey brother-cups-wrapper-common
 echo "installing Brother DCP-T710W Driver"
 makepkg -si
 
+echo "Installation Completed!!! "
+echo "NOTE Scanner won't be automatically activated !!!"
+echo "To Activate Scanner in Arch Linux in the terminal:"
+echo "brsaneconfig4 -a name='SCANNER_DCP-T710W' model='DCP-T710W' ip:<IP ADDRESS>"
+echo "Replace <IP ADDRESS> with your IP address"
